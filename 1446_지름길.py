4edbf7b3228f86f2a3a8d110251669fcb6c34619
kimheejoo <h32j00@gmail.com>
2021-03-19 23:59:46 +0900

    Dijkstra

--- 1446_지름길.py
+++ 1446_지름길.py
@@ -2,6 +2,7 @@ import sys
 
 N, D = map(int, sys.stdin.readline().split())
 arr = []
+
 for _ in range(N):
     s, e, leng = map(int, sys.stdin.readline().split())
     if e <= D:
@@ -14,7 +15,7 @@ for i in range(D+1):
         distance[i] = min(distance[i], distance[i-1]+1)
     for j in arr:
         if j[0] == i:
-            distance[j[1]] = min(distance[j[0]]+j[2], distance[j[1]])
+            distance[j[1]] = min(distance[j[0]] + j[2], distance[j[1]])
     print(distance)
 
 print(distance[-1])
